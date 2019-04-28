62c006a1b4f2cc6a2ecae70f88e90e6b9149bf4a
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-04-29 00:00:00 +0200

    Updated routes.

--- config/module.config.php
+++ config/module.config.php
@@ -1,11 +1,5 @@
 <?php
-
 namespace Adminer;
-
-use Zend\ServiceManager\Factory\InvokableFactory;
-
-$reader = new \Zend\Config\Reader\Ini();
-$db_config = $reader->fromFile(OMEKA_PATH . '/modules/Adminer/config/database.ini');
 
 return [
     'view_manager' => [
@@ -20,48 +14,18 @@ return [
     ],
     'controllers' => [
         'factories' => [
-            Controller\Admin\IndexController::class => InvokableFactory::class,
+            Controller\Admin\IndexController::class => \Zend\ServiceManager\Factory\InvokableFactory::class,
         ],
     ],
     'navigation' => [
         'AdminGlobal' => [
             [
-                'label'      => 'Adminer', // @translate
-                'route'      => 'admin/index',
-                'controller' => 'IndexController',
-                'action'     => 'index',
-                'pages' =>  [
-                    ($db_config['default_user_name'] !== '' && $db_config['default_user_password'] !== '')
-                    ? [
-                        'label' => 'Read only', // @translate
-                        'route' => 'admin/adminer',
-                        'controller' => 'access',
-                        'action' => 'browse',
-                        'target' => '_blank',
-                        'query' => [
-                            'server' => 'localhost',
-                            'username' => $db_config['default_user_name'],
-                            'password' => $db_config['default_user_password'],
-                            'db' => $db_config['db_name'],
-                        ],
-                    ]
-                    : null,
-                    ($db_config['main_user_name'] !== '' && $db_config['main_user_password'] !== '')
-                    ? [
-                        'label' => 'Full access', // @translate
-                        'route' => 'admin/adminer',
-                        'controller' => 'access',
-                        'action' => 'browse',
-                        'target' => '_blank',
-                        'query' => [
-                            'server' => 'localhost',
-                            'username' => $db_config['main_user_name'],
-                            'password' => $db_config['main_user_password'],
-                            'db' => $db_config['db_name'],
-                        ],
-                    ]
-                    : null
-                ],
+                'label' => 'Adminer', // @translate
+                'route' => 'admin/adminer',
+                'controller' => Controller\Admin\IndexController::class,
+                'action' => 'index',
+                // 'privilege' => 'browse',
+                'class' => '.o-icon-settings',
             ],
         ],
     ],
@@ -69,10 +33,10 @@ return [
         'routes' => [
             'admin' => [
                 'child_routes' => [
-                    'index' => [
+                    'adminer' => [
                         'type' => \Zend\Router\Http\Literal::class,
                         'options' => [
-                            'route' => '/adminer/index',
+                            'route' => '/adminer/manager',
                             'defaults' => [
                                 '__NAMESPACE__' => 'Adminer\Controller\Admin',
                                 'controller' => 'IndexController',
@@ -80,7 +44,7 @@ return [
                             ],
                         ],
                     ],
-                    'adminer' => [
+                    'adminer-mysql' => [
                         'type' => \Zend\Router\Http\Literal::class,
                         'options' => [
                             'route' => '/adminer',
